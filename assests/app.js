9cfb2d30a5f53b2fea3f154d9cb5b9ad25acffe3
Vaerilius <tjohannest@gmail.com>
2019-03-18 13:25:37 +0200

    all

--- assests/app.js
+++ assests/app.js
@@ -1,44 +1,26 @@
-$("ul").on('click','li', function() {
-  // console.log($(this).css('color'));
-  // if ($(this).css('color') === 'rgb(128, 128, 128)') {
-  // $(this).css({
-  //   color: "black",
-  //   textDecoration: "none"
-  // });
-
-  // } else {
-  //   $(this).css({
-  //     color: "gray",
-  //     textDecoration: "line-through"
-  //   });
-  // }
-
-  // add class to the css
-
-  $(this).toggleClass('compeleted');
-
-});
-// select ul --> if span is clicked run function
-$('ul').on('click', 'span', function(e) {
-  //parent il
-    $(this).parent().fadeOut(500, function () {
-      //this is parent element
-      $(this).remove();
-    });
-  // e.preventDafault()
-  e.stopPropagation();
+// Check Off Specific Todos By Clicking
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed");
 });
 
-$("input[type='text']").keypress(function(e) {
-  if (e.which === 13) {
-    let todoText= $(this).val();
-    $(this).val('');
-   $('ul').append(`<li><span><i class="far fa-trash-alt"></i> </span>${todoText}</li>`);
-
-  }
-  e.stopPropagation();
+//Click on X to delete Todo
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
 });
 
-$('#span').click(function () {
-  $('input[type"text"]').fadeOut();
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		//grabbing new todo text from input
+		var todoText = $(this).val();
+		$(this).val("");
+		//create a new li and add to ul
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
+	}
+});
+
+$("#toggle-form").click(function(){
+	$("input[type='text']").fadeToggle();
 });
